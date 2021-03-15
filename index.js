75780c763625827a63ca80fadcc1461052596c76
sanskarkhare <sanskarkhare47@gmail.com>
2021-03-16 01:27:54 +0545

    Google Authorisation

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const PORT = 5000;
+const db = require("./models");
+const { User } = require("./models");
 
-app.listen(PORT, () => (
-    console.log(`server running on port $(PORT)...`)
-))
+app.use(express.json());
+app.use(cors());
+
+app.post("/signin", (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const googleId = req.body.googleId;
+
+    User.create({
+        name: name,
+        email:email,
+        googleId: googleId,
+    }).catch((err) => {
+        console.log(err);
+    })
+
+    res.send("inserted into database")
+})
+
+
+db.sequelize.sync().then((req) => {
+    app.listen(PORT, () => (
+        console.log(`server running on port $(PORT)...`)
+    ))
+})
